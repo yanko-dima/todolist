d5d7a8849c6351e6ac922a94a93bd2fb500daf4c
Dmytro Yanko <yanko.dmitriy@gmail.com>
2022-11-03 23:51:36 +0200

    fix

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://62584f320c918296a49543e7.mockapi.io';
+axios.defaults.baseURL = 'https://63617cbaaf66cc87dc2ce66d.mockapi.io';
 
 export const fetchTasks = createAsyncThunk(
   'tasks/fetchAll',
